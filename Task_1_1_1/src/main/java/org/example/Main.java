9fdbba7348214c22e45d524e765848f9eca36edd
nkrainov <n.krainov@g.nsu.ru>
2024-09-03 19:18:46 +0700

    jacocoTestReport
    Added jacocoTestReport

--- Task_1_1_1/src/main/java/org/example/Main.java
+++ Task_1_1_1/src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] ans = HeapSort.sort(new int[] {5, 4, 3, 2, 8, 234, 5555, -134,4, 1});
+        int[] ans = HeapSort.sort(new int[] {5, 4, 3, 2, 8, 234, 5555, 134,4, 1});
         for (int i = 0; i< ans.length; i++){
             System.out.println(ans[i]);
         }

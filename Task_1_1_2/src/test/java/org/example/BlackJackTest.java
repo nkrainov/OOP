f2fb7e23c77d06ef5b23542c68ac5c04bb775c65
Nikita Krainov <n.krainov@g.nsu.ru>
2024-09-23 11:58:44 +0700

    added tests, returned main

--- Task_1_1_2/src/test/java/org/example/BlackJackTest.java
+++ Task_1_1_2/src/test/java/org/example/BlackJackTest.java
@@ -3,9 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.Scanner;
-
-class BlackJackTest {
+class Tests {
     @Test
     void testMoveAfterEndOfGame() {
         BlackJack game = new BlackJack();
@@ -26,20 +24,54 @@ class BlackJackTest {
             }
             Assertions.assertEquals(game.getSum(false), 0);
             Assertions.assertTrue(game.getCards(false).contains("<close card>"));
+            while (game.getCurStatusOfGame() == statusOfGame.playing) {
+                game.move(1);
+            }
+            Assertions.assertFalse(game.getCards(false).contains("<close card>"));
             break;
         }
     }
 
     @Test
-    void testGame(){
+    void testGetPlayersCards() {
+        BlackJack game = new BlackJack();
+        game.startRound();
+        Assertions.assertEquals(game.getCards(true).size(), 2);
+        int i = 2;
+        while (game.getCurStatusOfGame() == statusOfGame.playing) {
+            game.move(1);
+            i++;
+            Assertions.assertEquals(game.getCards(true).size(), i);
+        }
+
+    }
+
+    @Test
+    void testSum() {
+        BlackJack game = new BlackJack();
+        game.startRound();
+        while (game.getCurStatusOfGame() == statusOfGame.playing) {
+            game.move(1);
+        }
+        if (game.getCurStatusOfGame() == statusOfGame.dealerWin) {
+            Assertions.assertTrue(game.getSum(false) <= 21);
+        } else if (game.getCurStatusOfGame() == statusOfGame.playerWin) {
+            Assertions.assertTrue(game.getSum(true) <= 21);
+        } else if (game.getCurStatusOfGame() == statusOfGame.draw) {
+            Assertions.assertTrue(game.getSum(true) <= 21);
+            Assertions.assertTrue(game.getSum(false) <= 21);
+        }
+    }
+
+    @Test
+    void testGame() {
         BlackJack game = new BlackJack();
         int[] array = {1, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1,
-                       0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1};
+                0, 1, 1, 1, 0, 1, 0, 0, 0, 1, 1, 1};
         int index = 0;
         int prevDealerScore;
         int prevPlayerScore;
-
-        while (game.getDealerScore() < 2 && game.getPlayerScore() < 2){
+        while (game.getDealerScore() < 2 && game.getPlayerScore() < 2) {
             prevPlayerScore = game.getPlayerScore();
             prevDealerScore = game.getDealerScore();
             game.startRound();
@@ -47,18 +79,18 @@ class BlackJackTest {
                 game.move(array[index]);
                 index++;
             }
-            switch (game.getCurStatusOfGame()){
+            switch (game.getCurStatusOfGame()) {
                 case draw:
                     Assertions.assertEquals(prevDealerScore, game.getDealerScore());
                     Assertions.assertEquals(prevPlayerScore, game.getPlayerScore());
                     break;
                 case dealerWin:
-                    Assertions.assertEquals(prevDealerScore+1, game.getDealerScore());
+                    Assertions.assertEquals(prevDealerScore + 1, game.getDealerScore());
                     Assertions.assertEquals(prevPlayerScore, game.getPlayerScore());
                     break;
                 case playerWin:
                     Assertions.assertEquals(prevDealerScore, game.getDealerScore());
-                    Assertions.assertEquals(prevPlayerScore+1, game.getPlayerScore());
+                    Assertions.assertEquals(prevPlayerScore + 1, game.getPlayerScore());
                     break;
             }
         }

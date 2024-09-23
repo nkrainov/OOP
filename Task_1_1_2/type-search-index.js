146cffbdc10f9cc11d31beca7719538fde3a4cb7
nkrainov <nkrainov@users.noreply.github.com>
2024-09-23 03:53:31 +0000

    Deploying to gh-pages from @ nkrainov/OOP@3bda3875d4770f22cc5a06f1dae684e4647893d2 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"BlackJack"},{"p":"org.example","l":"Main"},{"p":"org.example","l":"StandartDeck"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"org.example","l":"BlackJack"},{"p":"org.example","l":"StandartDeck"}]

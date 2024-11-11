df986604185036663bf7fa4f94118d5d65af7460
Nikita Krainov <n.krainov@g.nsu.ru>
2024-11-11 11:44:10 +0700

    End

--- Task_1_2_2/src/main/java/org/example/Main.java
+++ Task_1_2_2/src/main/java/org/example/Main.java
@@ -2,7 +2,6 @@ package org.example;
 
 import org.hashtable.HashTable;
 
-import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
 public class Main {
@@ -10,31 +9,6 @@ public class Main {
         HashTable<Integer, String> h = new HashTable<>();
         h.put(1, "old");
         h.put(2, "new");
-        h.put(3, "irtegov");
-        h.put(4, "old");
-        h.put(5, "new");
-        h.put(6, "irtegov");
-        h.put(7, "old");
-        h.put(8, "new");
-        h.put(9, "irtegov");
-        h.put(10, "old");
-        h.put(21, "new");
-        h.put(31, "irtegov");
-        h.put(13, "old");
-        h.put(24, "new");
-        h.put(35, "irtegov");
-        h.put(16, "old");
-        h.put(27, "new");
-        h.put(38, "irtegov");
-        h.put(19, "old");
-        h.put(299, "new");
-        h.put(37, "irtegov");
-        h.put(15, "old");
-        h.put(265, "new");
-        h.put(3666, "irtegov");
-        h.put(1234, "old");
-        h.put(24342, "new");
-        h.put(3111, "irtegov");
         Iterator<HashTable.Pair<Integer, String>> f = h.getIterator();
         while (f.hasNext()) {
             System.out.println(f.next());
--- Task_1_2_2/src/main/java/org/hashtable/HashTable.java
+++ Task_1_2_2/src/main/java/org/hashtable/HashTable.java
@@ -1,16 +1,28 @@
 package org.hashtable;
 
-
 import java.util.ConcurrentModificationException;
 import java.util.Iterator;
 
+/**
+ * Класс, реализующий хэш-таблицу.
+ */
 public class HashTable<K, V> {
+
+    /**
+     * Класс, реализующий элемент хеш-таблицы, хранящий пару
+     * ключ-значение.
+     */
     public static class Pair<K, V> {
         final K key;
         V value;
         int hashcode;
         boolean alive;
+        String f;
 
+
+        /**
+         * Конструктор.
+         */
         Pair(K first, V second, int hashcode) {
             this.key = first;
             this.value = second;
@@ -18,32 +30,61 @@ public class HashTable<K, V> {
             this.alive = true;
         }
 
+        /**
+         * Преобразование в строковое представление.
+         */
         @Override
         public String toString() {
-            return "key " + key + ", value " + value + ".";
+            return "key: " + key + ", value: " + value + ".\n";
+        }
+
+        /**
+         * Получение ключа.
+         */
+        public K getKey() {
+            return key;
+        }
+
+        /**
+         * Получение значения.
+         */
+        public V getValue() {
+            return value;
         }
     }
+
     private Pair<?, ?>[] table;
     private int capacity = 16;
     private int curSize = 0;
     int modificationCount = 0;
+
+    /**
+     * Конструктор.
+     */
     public HashTable() {
         table = new Pair<?, ?>[capacity];
     }
 
+    /**
+     * Метод, кладущий в таблицу пару ключ-значение.
+     *
+     * @return Возвращает предыдущее значение по этому ключу.
+     */
     public V put(K key, V value) {
         if (key == null || value == null) {
             throw new NullPointerException("key or value is null");
         }
-        modificationCount++;
+
         if (curSize >= 0.75 * capacity) {
+            modificationCount++;
             resize();
         }
 
+        modificationCount++;
         int index = (key.hashCode() & 0x7FFFFFFF) % capacity;
         while (!(table[index] == null)) {
             if (table[index].hashcode == key.hashCode() && table[index].key.equals(key) &&
-            table[index].alive) {
+                    table[index].alive) {
                 Pair<K, V> pair = (Pair<K, V>) table[index];
                 V ans = pair.value;
                 pair.value = value;
@@ -57,27 +98,37 @@ public class HashTable<K, V> {
         return null;
     }
 
+    /**
+     * Метод, удаляющий из таблицы пару ключ-значение.
+     *
+     * @return Возвращает значение по этому ключу.
+     */
     public V remove(K key) {
         if (key == null) {
             throw new NullPointerException("key is null");
         }
 
-        modificationCount++;
         int index = (key.hashCode() & 0x7FFFFFFF) % capacity;
         while (!(table[index] == null)) {
-            if (table[index].hashcode == key.hashCode() && table[index].key.equals(key)) {
+            if (table[index].alive && table[index].hashcode == key.hashCode() && table[index].key.equals(key)) {
                 Pair<K, V> pair = (Pair<K, V>) table[index];
                 V ans = pair.value;
                 pair.alive = false;
+                modificationCount++;
                 return ans;
             }
             index = (index + 1) % table.length;
         }
 
-        table[index].alive = false;
         return null;
     }
 
+
+    /**
+     * Метод, позволяющий получить значение по этому ключу.
+     *
+     * @return Возвращает значение по этому ключу.
+     */
     public V get(K key) {
         if (key == null) {
             throw new NullPointerException("key is null");
@@ -93,26 +144,18 @@ public class HashTable<K, V> {
         return null;
     }
 
-    public boolean containsValue(V value) {
-        if (value == null) {
-            throw new NullPointerException("value is null");
-        }
-
-        for (int i = 0; i < capacity; i++) {
-            if (table[i].value.equals(value)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    public void update(K key, V value) {
+    /**
+     * Метод, обновляющий в таблице пару ключ-значение.
+     *
+     * @return Возвращает true при успешном обновлении, иначе false.
+     */
+    public boolean update(K key, V value) {
         if (key == null || value == null) {
             throw new NullPointerException("key or value is null");
         }
-        modificationCount++;
+
         if (curSize >= 0.75 * capacity) {
+            modificationCount++;
             resize();
         }
 
@@ -122,32 +165,64 @@ public class HashTable<K, V> {
                     table[index].alive) {
                 Pair<K, V> pair = (Pair<K, V>) table[index];
                 pair.value = value;
-                return;
+                modificationCount++;
+                return true;
             }
             index = (index + 1) % table.length;
         }
+
+        return false;
     }
 
+    /**
+     * Проверяет наличие пары с данным ключом.
+     *
+     * @return Если пара с таким ключом есть, то возвращает true
+     * иначе false.
+     */
+    public boolean search(K key) {
+        if (key == null) {
+            throw new NullPointerException("key is null");
+        }
+
+        int index = (key.hashCode() & 0x7FFFFFFF) % capacity;
+        while (!(table[index] == null)) {
+            if (table[index].hashcode == key.hashCode() && table[index].key.equals(key) &&
+                    table[index].alive) {
+                return true;
+            }
+            index = (index + 1) % table.length;
+        }
+
+        return false;
+    }
+
+    /**
+     * Преобразование в строковое представление.
+     */
     @Override
     public String toString() {
         StringBuilder ans = new StringBuilder();
         for (int i = 0; i < capacity; i++) {
             if (table[i] != null && table[i].alive) {
-                ans.append("key: ").append(table[i].key.toString()).append(" value: ").append(table[i].value.toString()).append("\n");
+                ans.append(table[i].toString());
             }
         }
         return ans.toString();
     }
 
+    /**
+     * Метод, увеличивающий таблицу в два раза.
+     */
     private void resize() {
-        Pair<?, ?>[] newTable = new Pair[capacity*2];
+        Pair<?, ?>[] newTable = new Pair[capacity * 2];
         int newSize = 0;
         for (int i = 0; i < capacity; i++) {
             if (table[i] != null && table[i].alive) {
                 newSize++;
-                int index = (table[i].key.hashCode() & 0x7FFFFFFF) % capacity*2;
+                int index = (table[i].key.hashCode() & 0x7FFFFFFF) % (capacity * 2);
                 while (!(newTable[index] == null)) {
-                    index = (index + 1) % (capacity*2);
+                    index = (index + 1) % (capacity * 2);
                 }
 
                 newTable[index] = new Pair<>(table[i].key, table[i].value, table[i].key.hashCode());
@@ -161,12 +236,15 @@ public class HashTable<K, V> {
 
     }
 
+    /**
+     * Проверка на равенство таблиц. Не симметричен!
+     */
     public boolean equals(Object o) {
         if (o == this) {
             return true;
         }
 
-        if (!(o instanceof HashTable<?,?>)) {
+        if (!(o instanceof HashTable<?, ?>)) {
             return false;
         }
 
@@ -183,19 +261,31 @@ public class HashTable<K, V> {
 
     }
 
-    public MyIterator<Pair<K, V>> getIterator() {
+    /**
+     * Возвращает итератор по этой таблице
+     */
+    public Iterator<Pair<K, V>> getIterator() {
         return new MyIterator<>(this);
     }
 
-    public class MyIterator<T> implements Iterator<T>{
+    /**
+     * Реализация итератора для этой таблицы.
+     */
+    public class MyIterator<T> implements Iterator<T> {
         private int curIndex = 0;
         private int curIndexSearch = 0;
         private final int memModificationCount;
 
-        public MyIterator (HashTable<K, V> tab) {
+        /**
+         * Конструктор.
+         */
+        public MyIterator(HashTable<K, V> tab) {
             this.memModificationCount = modificationCount;
         }
 
+        /**
+         * Проверка наличия следующего элемента.
+         */
         @Override
         public boolean hasNext() {
             if (memModificationCount != modificationCount) {
@@ -212,6 +302,9 @@ public class HashTable<K, V> {
             return false;
         }
 
+        /**
+         * Возвращает следующий элемент.
+         */
         @Override
         public T next() {
             if (memModificationCount != modificationCount) {
@@ -222,9 +315,12 @@ public class HashTable<K, V> {
                 return null;
             }
             curIndex = curIndexSearch;
-            return (T)table[curIndexSearch++];
+            return (T) table[curIndexSearch++];
         }
 
+        /**
+         * Удаление элемента из таблицы.
+         */
         @Override
         public void remove() {
             if (memModificationCount != modificationCount) {
@@ -234,7 +330,7 @@ public class HashTable<K, V> {
                 throw new NullPointerException();
             }
 
-            table[curIndex] = null;
+            table[curIndex].alive = false;
         }
 
     }
